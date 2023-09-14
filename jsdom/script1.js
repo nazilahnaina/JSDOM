d9b419ad0feb05916c14de30a504cfbb5a82d28b
nazilahnaina <nazilahnaina@gmail.com>
2023-09-14 13:19:49 +0700

    DOM manipulation

---
+++ jsdom/script1.js
@@ -0,0 +1,18 @@
+// //elment.innerHTML
+// const judul = document.getElementById('judul');
+// judul.innerHTML = '<i>Nazilah Naina Rachmawati</i>'
+
+// const sectionA = document.querySelector('section#a');
+// sectionA.innerHTML = 'Hello Naina...!!';
+
+// const sectionA = document.querySelector('section#a');
+// // sectionA.innerHTML = '<div><p>Paragraf Baru</p></div>';
+
+// const judul = document.getElementsByTagName('h1')[0];
+// judul.setAttribute('name','Nazilah Naina R.');
+
+const a= document.querySelector('section#a a');
+
+
+const p2 = document.querySelector('.p2');
+
