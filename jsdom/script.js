91e5ca4f7b6eeb36408e7ec43dc4867e9a750926
nazilahnaina <nazilahnaina@gmail.com>
2023-09-14 08:19:18 +0700

    baru

---
+++ jsdom/script.js
@@ -0,0 +1,53 @@
+const judul = document.getElementById('judul');
+judul.style.color='white';
+judul.style.backgroundColor='blue';
+judul.innerHTML = 'ninun';
+
+// const p = document.getElementsByTagName('p');
+//p[0].style.backgroundColor='lightblue';
+//p[1].style.backgroundColor='grey';
+//p[2].style.backgroundColor='lightyellow';
+//p[3].style.backgroundColor='lightgreen';
+
+for(let i=0;i<p.length; i++){
+    p[i].style.backgroundColor = 'lightblue';
+}
+
+//const h1 = document.getElementsByTagName('h1');
+//h1[0].style.fontSize='50px';
+//atau
+const h1 = document.getElementsByTagName('h1')[0];
+h1.style.fontSize='50px';
+
+const p1 = document.getElementsByClassName('p1');
+p1[0].innerHTML = "Dirubah dengan Javascript";
+
+//document.querySelector()
+// const p4 = document.querySelector('#b p');
+
+
+
+// memberikan warna dan membesarkan font p4
+const p4 = document.querySelector('#b p');
+p4.style.color = 'green';
+p4.style.fontSize = '25px';
+
+//mengubah background pada item2
+const li2 = document.querySelector('section#b ul li:nth-child(2)');
+li2.style.backgroundColor = 'lightcoral';
+
+// const p1 = document.querySelector('p');
+// const p4 = document.querySelector('#b p');
+// p1.innerHTML = 'ini dirubah melalui Javascript';
+
+//mengubah semua elemen p
+const p = document.querySelectorAll('p');
+p[2].style.backgroundColor='lightblue';
+
+const p = document.querySelectorAll('p');
+     for(let i=0;i<p.length; i++){
+        p[i].style.backgroundColor = 'lightgreen';
+     }
+
+
+
